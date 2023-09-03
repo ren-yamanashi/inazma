a820bbfc7749fb2417b899f3d91f979b79a9067d
Ren Yamanashi <96362223+ren-yamanashi@users.noreply.github.com>
2023-09-03 18:06:02 +0900

    add tls interface #5 (#6)
    Co-authored-by: ren_yamanashi <ren_yamanashi@mates-system.com>

---
+++ src/interfaces/tls.interface.ts
@@ -0,0 +1,34 @@
+interface KeyObject {
+  pem: string | Buffer;
+  passphrase?: string | undefined;
+}
+
+type SecureVersion = 'TLSv1.3' | 'TLSv1.2' | 'TLSv1.1' | 'TLSv1';
+
+interface PxfObject {
+  buf: string | Buffer;
+  passphrase?: string | undefined;
+}
+
+export interface SecureContextOptions {
+  ca?: string | Buffer | Array<string | Buffer> | undefined;
+  cert?: string | Buffer | Array<string | Buffer> | undefined;
+  sigalgs?: string | undefined;
+  ciphers?: string | undefined;
+  clientCertEngine?: string | undefined;
+  crl?: string | Buffer | Array<string | Buffer> | undefined;
+  dhparam?: string | Buffer | undefined;
+  ecdhCurve?: string | undefined;
+  honorCipherOrder?: boolean | undefined;
+  key?: string | Buffer | Array<string | Buffer | KeyObject> | undefined;
+  privateKeyEngine?: string | undefined;
+  maxVersion?: SecureVersion | undefined;
+  minVersion?: SecureVersion | undefined;
+  passphrase?: string | undefined;
+  pfx?: string | Buffer | Array<string | Buffer | PxfObject> | undefined;
+  secureOptions?: number | undefined;
+  secureProtocol?: string | undefined;
+  sessionIdContext?: string | undefined;
+  ticketKeys?: Buffer | undefined;
+  sessionTimeout?: number | undefined;
+}
