e31e558f8660f591b1b5fef6e9955cf32815c2cd
ren_yamanashi <ren_yamanashi@mates-system.com>
2023-09-03 18:05:20 +0900

    add tls interface #5

--- src/interfaces/mysql.interface.ts
+++ src/interfaces/mysql.interface.ts
@@ -1,6 +1,5 @@
-import * as events from 'events';
 import * as stream from 'stream';
-import * as tls from 'tls';
+import { SecureContextOptions } from './tls.interface';
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
@@ -19,40 +18,40 @@ type TypeCast =
 
 type packetCallback = (packet: any) => void;
 
-export type queryCallback = (err: MysqlError | null, results?: any, fields?: FieldInfo[]) => void;
+type queryCallback = (err: MysqlError | null, results?: any, fields?: FieldInfo[]) => void;
 
 enum Types {
-  DECIMAL = 0x00, // aka DECIMAL (http://dev.mysql.com/doc/refman/5.0/en/precision-math-decimal-changes.html)
-  TINY = 0x01, // aka TINYINT, 1 byte
-  SHORT = 0x02, // aka SMALLINT, 2 bytes
-  LONG = 0x03, // aka INT, 4 bytes
-  FLOAT = 0x04, // aka FLOAT, 4-8 bytes
-  DOUBLE = 0x05, // aka DOUBLE, 8 bytes
-  NULL = 0x06, // NULL (used for prepared statements, I think)
-  TIMESTAMP = 0x07, // aka TIMESTAMP
-  LONGLONG = 0x08, // aka BIGINT, 8 bytes
-  INT24 = 0x09, // aka MEDIUMINT, 3 bytes
-  DATE = 0x0a, // aka DATE
-  TIME = 0x0b, // aka TIME
-  DATETIME = 0x0c, // aka DATETIME
-  YEAR = 0x0d, // aka YEAR, 1 byte (don't ask)
-  NEWDATE = 0x0e, // aka ?
-  VARCHAR = 0x0f, // aka VARCHAR (?)
-  BIT = 0x10, // aka BIT, 1-8 byte
-  TIMESTAMP2 = 0x11, // aka TIMESTAMP with fractional seconds
-  DATETIME2 = 0x12, // aka DATETIME with fractional seconds
-  TIME2 = 0x13, // aka TIME with fractional seconds
-  JSON = 0xf5, // aka JSON
-  NEWDECIMAL = 0xf6, // aka DECIMAL
-  ENUM = 0xf7, // aka ENUM
-  SET = 0xf8, // aka SET
-  TINY_BLOB = 0xf9, // aka TINYBLOB, TINYTEXT
-  MEDIUM_BLOB = 0xfa, // aka MEDIUMBLOB, MEDIUMTEXT
-  LONG_BLOB = 0xfb, // aka LONGBLOG, LONGTEXT
-  BLOB = 0xfc, // aka BLOB, TEXT
-  VAR_STRING = 0xfd, // aka VARCHAR, VARBINARY
-  STRING = 0xfe, // aka CHAR, BINARY
-  GEOMETRY = 0xff, // aka GEOMETRY
+  DECIMAL = 0x00,
+  TINY = 0x01,
+  SHORT = 0x02,
+  LONG = 0x03,
+  FLOAT = 0x04,
+  DOUBLE = 0x05,
+  NULL = 0x06,
+  TIMESTAMP = 0x07,
+  LONGLONG = 0x08,
+  INT24 = 0x09,
+  DATE = 0x0a,
+  TIME = 0x0b,
+  DATETIME = 0x0c,
+  YEAR = 0x0d,
+  NEWDATE = 0x0e,
+  VARCHAR = 0x0f,
+  BIT = 0x10,
+  TIMESTAMP2 = 0x11,
+  DATETIME2 = 0x12,
+  TIME2 = 0x13,
+  JSON = 0xf5,
+  NEWDECIMAL = 0xf6,
+  ENUM = 0xf7,
+  SET = 0xf8,
+  TINY_BLOB = 0xf9,
+  MEDIUM_BLOB = 0xfa,
+  LONG_BLOB = 0xfb,
+  BLOB = 0xfc,
+  VAR_STRING = 0xfd,
+  STRING = 0xfe,
+  GEOMETRY = 0xff,
 }
 
 interface GeometryType extends Array<{ x: number; y: number } | GeometryType> {
@@ -80,7 +79,7 @@ interface UntypedFieldInfo {
   protocol41: boolean;
 }
 
-export interface ConnectionOptions {
+interface ConnectionOptions {
   user?: string | undefined;
   password?: string | undefined;
   database?: string | undefined;
@@ -88,63 +87,13 @@ export interface ConnectionOptions {
   timeout?: number | undefined;
 }
 
-export interface MysqlConnectionConfig extends ConnectionOptions {
-  host?: string | undefined;
-  port?: number | undefined;
-  localAddress?: string | undefined;
-  socketPath?: string | undefined;
-  timezone?: string | undefined;
-  connectTimeout?: number | undefined;
-  stringifyObjects?: boolean | undefined;
-  insecureAuth?: boolean | undefined;
-  typeCast?: TypeCast | undefined;
-  queryFormat?(query: string, values: any): string;
-  supportBigNumbers?: boolean | undefined;
-  bigNumberStrings?: boolean | undefined;
-  dateStrings?: boolean | Array<'TIMESTAMP' | 'DATETIME' | 'DATE'> | undefined;
-  trace?: boolean | undefined;
-  multipleStatements?: boolean | undefined;
-  flags?: string | string[] | undefined;
-  debug?: boolean | string[] | Types[] | undefined | any;
-  ssl?:
-    | string
-    | (tls.SecureContextOptions & { rejectUnauthorized?: boolean | undefined })
-    | undefined;
-}
-
-export interface PoolSpecificConfig {
-  /**
-   * The milliseconds before a timeout occurs during the connection acquisition. This is slightly different from connectTimeout,
-   * because acquiring a pool connection does not always involve making a connection. (Default: 10 seconds)
-   */
-  acquireTimeout?: number | undefined;
-
-  /**
-   * Determines the pool's action when no connections are available and the limit has been reached. If true, the pool will queue
-   * the connection request and call it when one becomes available. If false, the pool will immediately call back with an error.
-   * (Default: true)
-   */
-  waitForConnections?: boolean | undefined;
-
-  /**
-   * The maximum number of connections to create at once. (Default: 10)
-   */
-  connectionLimit?: number | undefined;
-
-  /**
-   * The maximum number of connection requests the pool will queue before returning an error from getConnection. If set to 0, there
-   * is no limit to the number of queued connection requests. (Default: 0)
-   */
-  queueLimit?: number | undefined;
-}
-
-export interface EscapeFunctions {
+interface EscapeFunctions {
   escape(value: any, stringifyObjects?: boolean, timeZone?: string): string;
   escapeId(value: string, forbidQualified?: boolean): string;
   format(sql: string, values: any[], stringifyObjects?: boolean, timeZone?: string): string;
 }
 
-export interface MysqlError extends Error {
+interface MysqlError extends Error {
   code: string;
   errno: number;
   sqlStateMarker?: string | undefined;
@@ -155,7 +104,7 @@ export interface MysqlError extends Error {
   sqlMessage?: string | undefined;
 }
 
-export interface Query {
+interface Query {
   sql: string;
   values?: string[] | undefined;
   typeCast?: TypeCast | undefined;
@@ -167,30 +116,17 @@ export interface Query {
   ResultSetHeaderPacket: packetCallback;
   FieldPacket: packetCallback;
   EofPacket: packetCallback;
-
   RowDataPacket(packet: any, parser: any, connection: MysqlConnection): void;
-
-  /**
-   * Creates a Readable stream with the given options
-   *
-   * @param options The options for the stream. (see readable-stream package)
-   */
   stream(options?: stream.ReadableOptions): stream.Readable;
-
   on(ev: string, callback: (...args: any[]) => void): Query;
-
   on(ev: 'result', callback: (row: any, index: number) => void): Query;
-
   on(ev: 'error', callback: (err: MysqlError) => void): Query;
-
   on(ev: 'fields', callback: (fields: FieldInfo[], index: number) => void): Query;
-
   on(ev: 'packet', callback: (packet: any) => void): Query;
-
   on(ev: 'end', callback: () => void): Query;
 }
 
-export interface QueryOptions {
+interface QueryOptions {
   sql: string;
   values?: any;
   timeout?: number | undefined;
@@ -198,15 +134,13 @@ export interface QueryOptions {
   typeCast?: TypeCast | undefined;
 }
 
-export interface QueryFunction {
+interface QueryFunction {
   (query: Query): Query;
-
   (options: string | QueryOptions, callback?: queryCallback): Query;
-
   (options: string | QueryOptions, values: any, callback?: queryCallback): Query;
 }
 
-export interface MysqlConnection extends EscapeFunctions, events.EventEmitter {
+export interface MysqlConnection extends EscapeFunctions, NodeJS.EventEmitter {
   config: MysqlConnectionConfig;
   state: 'connected' | 'authenticated' | 'disconnected' | 'protocol_error' | string;
   threadId: number | null;
@@ -231,6 +165,27 @@ export interface MysqlConnection extends EscapeFunctions, events.EventEmitter {
   destroy(): void;
   pause(): void;
   resume(): void;
+}
+
+export interface MysqlConnectionConfig extends ConnectionOptions {
+  host?: string | undefined;
+  port?: number | undefined;
+  localAddress?: string | undefined;
+  socketPath?: string | undefined;
+  timezone?: string | undefined;
+  connectTimeout?: number | undefined;
+  stringifyObjects?: boolean | undefined;
+  insecureAuth?: boolean | undefined;
+  typeCast?: TypeCast | undefined;
+  queryFormat?(query: string, values: any): string;
+  supportBigNumbers?: boolean | undefined;
+  bigNumberStrings?: boolean | undefined;
+  dateStrings?: boolean | Array<'TIMESTAMP' | 'DATETIME' | 'DATE'> | undefined;
+  trace?: boolean | undefined;
+  multipleStatements?: boolean | undefined;
+  flags?: string | string[] | undefined;
+  debug?: boolean | string[] | Types[] | undefined | any;
+  ssl?: string | (SecureContextOptions & { rejectUnauthorized?: boolean | undefined }) | undefined;
 }
 
 export interface MysqlClientInterface {
