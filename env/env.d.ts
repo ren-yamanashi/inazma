cacb87c49626faaee866d4f56f03e160a3934195
ren_yamanashi <ren_yamanashi@mates-system.com>
2023-09-14 07:28:50 +0900

    env safe type #10

---
+++ env/env.d.ts
@@ -0,0 +1,6 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    MYSQL_USERNAME: string;
+    MYSQL_PASSWORD: string;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,4 @@
 import { schemaGen } from './commands/schemaGen.command';
-import {
-  autoIncrementColumnDecorator,
-  columnDecorator,
-  defaultGeneratedColumnDecorator,
-  onUpdateCurrentTimestampColumnDecorator,
-  storedGeneratedColumnDecorator,
-  virtualGeneratedColumnDecorator,
-} from './decorators/column.decorator';
-import { entityDecorator, indexDecorator } from './decorators/table.decorator';
 import { registerContainer } from './di';
 import { getNowDate } from './helpers/datetime';
 
@@ -16,8 +7,8 @@ import { getNowDate } from './helpers/datetime';
 
   await schemaGen({
     host: 'localhost',
-    user: 'root',
-    password: 'password',
+    user: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
     database: 'sample',
   });
 
@@ -25,23 +16,7 @@ import { getNowDate } from './helpers/datetime';
 })();
 
 export const NOW = getNowDate;
-export const Column = columnDecorator;
-export const AutoIncrementColumn = autoIncrementColumnDecorator;
-export const VirtualGeneratedColumn = virtualGeneratedColumnDecorator;
-export const StoredGeneratedColumn = storedGeneratedColumnDecorator;
-export const DefaultGeneratedColumn = defaultGeneratedColumnDecorator;
-export const OnUpdateCurrentTimestampColumn = onUpdateCurrentTimestampColumnDecorator;
-export const Index = indexDecorator;
-export const Entity = entityDecorator;
 
 export default {
   NOW,
-  Column,
-  AutoIncrementColumn,
-  VirtualGeneratedColumn,
-  StoredGeneratedColumn,
-  DefaultGeneratedColumn,
-  OnUpdateCurrentTimestampColumn,
-  Index,
-  Entity,
 };
